bb829e5b41f68a84e6774cab9b359414bdf4e621
stianbm <stian.malmanger@gmail.com>
2019-01-17 17:56:11 +0100

    Started on special case

--- Project1/core/src/com/mygdx/game/MyGdxGame.java
+++ Project1/core/src/com/mygdx/game/MyGdxGame.java
@@ -21,7 +21,6 @@ public class MyGdxGame extends ApplicationAdapter {
     @Override
     public void create() {
 
-        //TODO can optimze by making list of texture instead of names
         // Add images to list
         imageNames.add(new Texture("heli1.png"));
         imageNames.add(new Texture("heli2.png"));
@@ -30,12 +29,12 @@ public class MyGdxGame extends ApplicationAdapter {
 
 
         // Add three chopper entities
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < 3; i++) {
             Chopper c = new Chopper(/*
                     new Vector2(i*200, 0), // Different position
                     new Vector2(i + 1, i + 1), // different directions*/
-                    new Vector2(20, i*300), // Different position
-                    new Vector2(i+1,  1), // different directions
+                    new Vector2(i*70, i*200), // Different position
+                    new Vector2(1,  1), // different directions
                     imageNames
             );
 
@@ -86,21 +85,29 @@ public class MyGdxGame extends ApplicationAdapter {
 
                 c.updatePosition();
 
+                //Iterate through the choppers
                 for (Chopper chopper : choppers) {
+                    //Don't compare with self
                     if (c != chopper) {
+                        // Check if collision
                         if(collideX(c, chopper) && collideY(c, chopper)){
-                            if(abs(c.getPosition().x - chopper.getPosition().x)
-                                    > abs(c.getPosition().y - chopper.getPosition().y)){
+                            //Base the direction change on the distance between the positions in x and y direction
+                            if(abs(c.getPosition().x - chopper.getPosition().x)*c.getImg().getHeight()/c.getImg().getWidth()
+                                    < abs(c.getPosition().y - chopper.getPosition().y)){
+                                if(c.getVelocity().y * chopper.getVelocity().y < 0){
+                                    //TODO fix
+                                }
                                 c.changeDirectionY();
                                 chopper.changeDirectionY();
                                 System.out.println("Y");
-                            } else if(abs(c.getPosition().x - chopper.getPosition().x)
-                                    < abs(c.getPosition().y - chopper.getPosition().y)){
+                            } else/* if(abs(c.getPosition().x - chopper.getPosition().x)*c.getImg().getHeight()/c.getImg().getWidth()
+                                    < abs(c.getPosition().y - chopper.getPosition().y))*/{
+                                    //TODO fix
                                 c.changeDirectionX();
                                 chopper.changeDirectionX();
                                 System.out.println("X");
                             }
-
+                            /*
                             else{
                                 c.changeDirectionX();
                                 chopper.changeDirectionX();
@@ -108,6 +115,7 @@ public class MyGdxGame extends ApplicationAdapter {
                                 chopper.changeDirectionY();
                                 System.out.println("XY");
                             }
+                            */
                         }
                     }
                 }
@@ -140,10 +148,7 @@ public class MyGdxGame extends ApplicationAdapter {
         }
     }
 
-    //TODO add collision detect and correct
-    //Check for collision towards all other choppers: new for loop.
-    //Check must take into account what parts are colliding.
-    //Left wall will always collide with right wall, bottom with top.
+    // Check for overlap in x-direction
     private boolean collideX(Chopper chopper1, Chopper chopper2) {
         if(chopper1.getPosition().x >= chopper2.getPosition().x
                 && chopper1.getPosition().x <= chopper2.getPosition().x + chopper2.getImg().getWidth())
@@ -153,6 +158,7 @@ public class MyGdxGame extends ApplicationAdapter {
         else{return false;}
     }
 
+    // Check for overlap in y-direction
     private boolean collideY(Chopper chopper1, Chopper chopper2) {
         if(chopper1.getPosition().y >= chopper2.getPosition().y
                 && chopper1.getPosition().y <= chopper2.getPosition().y + chopper2.getImg().getHeight()){
