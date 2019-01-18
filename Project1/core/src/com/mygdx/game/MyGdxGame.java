6b4b2eb2bc91f2f5d5340630425808a78bd47110
stianbm <stian.malmanger@gmail.com>
2019-01-18 14:02:34 +0100

    Added description and classes

--- Project1/core/src/com/mygdx/game/MyGdxGame.java
+++ Project1/core/src/com/mygdx/game/MyGdxGame.java
@@ -4,6 +4,15 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 
+/* PONG
+Make a ball, score, two bars and one controller.
+The bars and controller behave differently -> split into two classes.
+Ball bounces off floor, ceiling and bars.
+Game checks if someone scored by checking the x-coordinate of the ball against the borders of the
+    screen, if so it updates the score and re-spawns the ball.
+Controller controls left bar.
+Right bar is simple AI with max speed that centers bar on the y-coordinate of the ball
+ */
 public class MyGdxGame extends ApplicationAdapter {
     private long currentTime = System.nanoTime();
     private long accumulator;
